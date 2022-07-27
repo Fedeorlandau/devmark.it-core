47306b365e012f7c052eb1d7b87ce4fde5475f99
Federico Orlandau <federico.orlandau@gmail.com>
2022-07-27 19:53:54 +0200

    feat: refactor socket connection

---
+++ apps/frontend/src/lib/socket.ts
@@ -0,0 +1,47 @@
+import { io, Socket } from "socket.io-client";
+import { variables } from "@lib/variables";
+
+interface UseSocketProps {
+  roomId: string;
+  onUpdated?: (event: { name: string; data: any }) => void;
+  onException?: () => void;
+  onConnect?: () => void;
+  onJoined?: () => void;
+  onExpired?: () => void;
+}
+
+export default function initSocket({
+  roomId,
+  onUpdated,
+  onException,
+  onConnect,
+  onJoined,
+  onExpired,
+}: UseSocketProps): Socket {
+  const socket = io(variables.wsUrl);
+
+  socket.on("connect", function () {
+    socket.emit("events", { type: "join", payload: { id: roomId } });
+    onConnect?.();
+  });
+
+  socket.on("updated", function (event) {
+    onUpdated?.(event);
+  });
+
+  socket.on("exception", function (event) {
+    console.log("event", event);
+    onException?.();
+  });
+
+  socket.on("joined", function () {
+    onJoined?.();
+  });
+
+  socket.on("expire", function () {
+    socket.close();
+    onExpired?.();
+  });
+
+  return socket;
+}
