c04a642261915f50513712c858f84dd711f33e4e
Federico Orlandau <federico.orlandau@gmail.com>
2022-08-17 12:42:33 +0200

    feat: add connection to web socket

---
+++ apps/astro/src/utils/socket.ts
@@ -0,0 +1,46 @@
+import { io, Socket } from "socket.io-client";
+
+interface UseSocketProps {
+  roomId: string;
+  onUpdated?: (event: { name: string; data: any }) => void;
+  onException?: () => void;
+  onConnect?: () => void;
+  onJoined?: () => void;
+  onExpired?: () => void;
+}
+
+export default function initSocket({
+  roomId,
+  onUpdated,
+  onException,
+  onConnect,
+  onJoined,
+  onExpired,
+}: UseSocketProps): Socket {
+  const socket = io(import.meta.env.PUBLIC_WS_URL);
+
+  socket.on("connect", function () {
+    socket.emit("events", { type: "join", payload: { id: roomId } });
+    onConnect?.();
+  });
+
+  socket.on("updated", function (event) {
+    onUpdated?.(event);
+  });
+
+  socket.on("exception", function (event) {
+    console.log("event", event);
+    onException?.();
+  });
+
+  socket.on("joined", function () {
+    onJoined?.();
+  });
+
+  socket.on("expire", function () {
+    socket.close();
+    onExpired?.();
+  });
+
+  return socket;
+}
